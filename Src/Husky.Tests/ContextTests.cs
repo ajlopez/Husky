7966f53ee7fd13c221f71fe062cc0b0c7cace920
ajlopez <webmaster@ajlopez.com>
2015-09-27 19:02:27 -0300

    Context get undefined value as null

---
+++ Src/Husky.Tests/ContextTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Husky.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ContextTests
+    {
+        [TestMethod]
+        public void GetUndefinedValueAsNull()
+        {
+            var ctx = new Context();
+
+            Assert.IsNull(ctx.GetValue("foo"));
+        }
+    }
+}
