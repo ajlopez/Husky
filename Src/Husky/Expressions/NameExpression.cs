fb7a1cc0b2ec6a2343db0a9170b3cbf1084d318f
ajlopez <webmaster@ajlopez.com>
2015-09-29 06:39:14 -0300

    Name expression

---
+++ Src/Husky/Expressions/NameExpression.cs
@@ -0,0 +1,24 @@
+﻿namespace Husky.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class NameExpression
+    {
+        private string name;
+
+        public NameExpression(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name { get { return this.name; } }
+
+        public object Evaluate(Context ctx)
+        {
+            return ctx.GetValue(this.name);
+        }
+    }
+}
