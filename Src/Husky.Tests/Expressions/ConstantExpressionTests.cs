aba2f9fb87366e4aa6136566f906c94144ba5de8
ajlopez <webmaster@ajlopez.com>
2015-09-30 06:59:20 -0300

    Constant expression

---
+++ Src/Husky.Tests/Expressions/ConstantExpressionTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Husky.Tests.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Husky.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ConstantExpressionTests
+    {
+        [TestMethod]
+        public void GetIntegerConstant()
+        {
+            var expr = new ConstantExpression(42);
+
+            Assert.AreEqual(42, expr.Value);
+            Assert.AreEqual(42, expr.Evaluate(null));
+        }
+    }
+}
