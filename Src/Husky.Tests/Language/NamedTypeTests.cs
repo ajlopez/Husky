f8777b99efbd5de15924600370781359372abff0
ajlopez <webmaster@ajlopez.com>
2015-10-02 06:58:58 -0300

    Fixing Source Analysis

--- Src/Husky.Tests/Language/NamedTypeTests.cs
+++ Src/Husky.Tests/Language/NamedTypeTests.cs
@@ -1,11 +1,11 @@
 ﻿namespace Husky.Tests.Language
 {
     using System;
-    using System.Text;
     using System.Collections.Generic;
     using System.Linq;
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Text;
     using Husky.Language;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
     public class NamedTypeTests
