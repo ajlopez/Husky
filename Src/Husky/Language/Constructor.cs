8c67329618251c2fdd63fdd0ca19870a5ca6b685
ajlopez <webmaster@ajlopez.com>
2015-10-03 07:46:58 -0300

    Create constructor

---
+++ Src/Husky/Language/Constructor.cs
@@ -0,0 +1,23 @@
+﻿namespace Husky.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Constructor
+    {
+        private NamedType type;
+        private string name;
+
+        public Constructor(string name, NamedType type)
+        {
+            this.name = name;
+            this.type = type;
+        }
+
+        public string Name { get { return this.name; } }
+
+        public NamedType Type { get { return this.type; } }
+    }
+}
