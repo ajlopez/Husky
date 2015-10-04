884dd77f08c8b0de4b7d55532f2744658231508f
ajlopez <webmaster@ajlopez.com>
2015-10-04 12:23:03 -0300

    Constructor expression

---
+++ Src/Husky.Tests/Expressions/ConstructorExpressionTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Husky.Tests.Expressions
+{
+    using System;
+    using Husky.Expressions;
+    using Husky.Types;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ConstructorExpressionTests
+    {
+        [TestMethod]
+        public void CreateConstructor()
+        {
+            var cons = new ConstructorExpression(IntegerType.Instance);
+
+            Assert.AreSame(IntegerType.Instance, cons.Type);
+        }
+    }
+}
