3830643ba06e5c1156aa5700379db3df90c588c9
ajlopez <webmaster@ajlopez.com>
2015-10-04 16:00:39 -0300

    Constructor function and Instance expression, WIP

---
+++ Src/Husky/Expressions/InstanceExpression.cs
@@ -0,0 +1,28 @@
+﻿namespace Husky.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Husky.Functions;
+    using Husky.Types;
+
+    public class InstanceExpression : IExpression
+    {
+        private ConstructorFunction consfn;
+        private IList<IExpression> exprs;
+
+        public InstanceExpression(ConstructorFunction consfn, IList<IExpression> exprs)
+        {
+            this.consfn = consfn;
+            this.exprs = exprs;
+        }
+
+        public IType Type { get { return this.consfn.Type; } }
+
+        public IExpression Reduce()
+        {
+            return this;
+        }
+    }
+}
---
+++ Src/Husky/Functions/ConstructorFunction.cs
@@ -0,0 +1,33 @@
+﻿namespace Husky.Functions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Husky.Expressions;
+    using Husky.Types;
+
+    public class ConstructorFunction : IFunction
+    {
+        private IType type;
+        private IList<IType> types;
+
+        public ConstructorFunction(IType type, IList<IType> types)
+        {
+            this.type = type;
+            this.types = types;
+        }
+
+        public IType Type { get { return this.type; } }
+
+        public IExpression Reduce()
+        {
+            return this; 
+        }
+
+        public IExpression Apply(IList<IExpression> exprs)
+        {
+            return new InstanceExpression(this, exprs);
+        }
+    }
+}
