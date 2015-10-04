b3d9b5739e5fae9f0ca06ea1b34dc9c752ccd8a1
ajlopez <webmaster@ajlopez.com>
2015-10-04 14:01:52 -0300

    Add integers function first test

---
+++ Src/Husky.Tests/Expressions/ArithmeticFunctionTests.cs
@@ -0,0 +1,27 @@
+﻿namespace Husky.Tests.Expressions
+{
+    using System;
+    using Husky.Expressions;
+    using Husky.Functions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ArithmeticFunctionTests
+    {
+        [TestMethod]
+        public void AddIntegers()
+        {
+            Assert.AreEqual(3, Add(1, 2));
+        }
+
+        private static int Add(int x, int y) 
+        {
+            IExpression exprx = new IntegerExpression(x);
+            IExpression expry = new IntegerExpression(y);
+
+            IExpression expr = (new AddIntegersFunction()).Apply(new IExpression[] { exprx, expry });
+
+            return ((IntegerExpression)expr).Value;
+        }
+    }
+}
---
+++ Src/Husky/Functions/AddIntegersFunction.cs
@@ -0,0 +1,20 @@
+﻿namespace Husky.Functions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Husky.Expressions;
+
+    public class AddIntegersFunction
+    {
+        public AddIntegersFunction()
+        {
+        }
+
+        public IExpression Apply(IList<IExpression> exprs)
+        {
+            return new IntegerExpression(((IntegerExpression)exprs[0]).Value + ((IntegerExpression)exprs[1]).Value); 
+        }
+    }
+}
