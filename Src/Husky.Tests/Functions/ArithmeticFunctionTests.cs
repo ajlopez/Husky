b492ff0736bc401ce6721fecb5771500ae130ff7
ajlopez <webmaster@ajlopez.com>
2015-10-05 07:07:08 -0300

    Subtract integers function

---
+++ Src/Husky.Tests/Functions/ArithmeticFunctionTests.cs
@@ -0,0 +1,57 @@
+﻿namespace Husky.Tests.Functions
+{
+    using System;
+    using Husky.Expressions;
+    using Husky.Functions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ArithmeticFunctionTests
+    {
+        [TestMethod]
+        public void AddIntegers()
+        {
+            Assert.AreEqual(3, Add(1, 2));
+            Assert.AreEqual(-1, Add(1, -2));
+            Assert.AreEqual(42, Add(44, -2));
+        }
+
+        [TestMethod]
+        public void SubtractIntegers()
+        {
+            Assert.AreEqual(-1, Subtract(1, 2));
+            Assert.AreEqual(3, Subtract(1, -2));
+            Assert.AreEqual(42, Subtract(44, 2));
+        }
+
+        [TestMethod]
+        public void AddThreeIntegersUsingFunctionalExpressions()
+        {
+            IExpression fn = new AddIntegersFunction();
+
+            IExpression expr = new FunctionalExpression(fn, new IExpression[] { new FunctionalExpression(fn, new IExpression[] { new IntegerExpression(1), new IntegerExpression(2) }), new IntegerExpression(3) });
+
+            Assert.AreEqual(new IntegerExpression(6), expr.Reduce());
+        }
+
+        private static int Add(int x, int y) 
+        {
+            IExpression exprx = new IntegerExpression(x);
+            IExpression expry = new IntegerExpression(y);
+
+            IExpression expr = (new AddIntegersFunction()).Apply(new IExpression[] { exprx, expry });
+
+            return ((IntegerExpression)expr).Value;
+        }
+
+        private static int Subtract(int x, int y)
+        {
+            IExpression exprx = new IntegerExpression(x);
+            IExpression expry = new IntegerExpression(y);
+
+            IExpression expr = (new SubtractIntegersFunction()).Apply(new IExpression[] { exprx, expry });
+
+            return ((IntegerExpression)expr).Value;
+        }
+    }
+}
---
+++ Src/Husky/Functions/SubtractIntegersFunction.cs
@@ -0,0 +1,30 @@
+﻿namespace Husky.Functions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Husky.Expressions;
+    using Husky.Types;
+
+    public class SubtractIntegersFunction : IFunction
+    {
+        private static IType type = new MapType(IntegerType.Instance, new MapType(IntegerType.Instance, IntegerType.Instance));
+
+        public SubtractIntegersFunction()
+        {
+        }
+
+        public IExpression Apply(IList<IExpression> exprs)
+        {
+            return new IntegerExpression(((IntegerExpression)(exprs[0].Reduce())).Value - ((IntegerExpression)(exprs[1].Reduce())).Value); 
+        }
+
+        public IType Type { get { return type; } }
+
+        public IExpression Reduce()
+        {
+            return this;
+        }
+    }
+}
