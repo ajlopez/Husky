d59ae883c94f2eb5ef5066bd137ed20ddd7760b4
ajlopez <webmaster@ajlopez.com>
2015-10-10 16:55:40 -0300

    NameExpression first test

---
+++ Src/Husky.Tests/Expressions/NameExpressionTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Husky.Tests.Expressions
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Husky.Expressions;
+
+    [TestClass]
+    public class NameExpressionTests
+    {
+        [TestMethod]
+        public void CreateNameExpression()
+        {
+            var expr = new NameExpression("a");
+
+            Assert.AreEqual("a", expr.Name);
+            Assert.IsNull(expr.Type);
+        }
+    }
+}
---
+++ Src/Husky/Expressions/NameExpression.cs
@@ -0,0 +1,28 @@
+﻿namespace Husky.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Husky.Types;
+
+    public class NameExpression : IExpression
+    {
+        private string name;
+        private IType type;
+
+        public NameExpression(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name { get { return this.name; } }
+
+        public IType Type { get { return this.type; } }
+
+        public IExpression Reduce()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
