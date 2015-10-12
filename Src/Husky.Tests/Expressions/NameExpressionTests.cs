c4b50eaea16709a9417e267450489b5892c0694e
ajlopez <webmaster@ajlopez.com>
2015-10-12 07:24:22 -0300

    Fixing source analysis

--- Src/Husky.Tests/Expressions/NameExpressionTests.cs
+++ Src/Husky.Tests/Expressions/NameExpressionTests.cs
@@ -1,11 +1,11 @@
 ﻿namespace Husky.Tests.Expressions
 {
     using System;
-    using System.Text;
     using System.Collections.Generic;
     using System.Linq;
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Text;
     using Husky.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
     public class NameExpressionTests
