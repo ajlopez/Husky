31a581089ea6b503629b2b2ced6e79c69d68bcb8
ajlopez <webmaster@ajlopez.com>
2015-10-14 07:05:10 -0300

    Lexer first tests

---
+++ Src/Husky.Tests/Compiler/LexerTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Husky.Tests.Compiler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Husky.Compiler;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class LexerTests
+    {
+        [TestMethod]
+        public void GetNullTokenFromNullText()
+        {
+            Lexer lexer = new Lexer(null);
+
+            Assert.IsNull(lexer.NextToken());
+        }
+
+        [TestMethod]
+        public void GetNullTokenFromEmptyText()
+        {
+            Lexer lexer = new Lexer(string.Empty);
+
+            Assert.IsNull(lexer.NextToken());
+        }
+    }
+}
