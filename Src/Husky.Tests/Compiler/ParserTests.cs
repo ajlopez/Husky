025dcb79be904e629a80d51fa8c2efcef3c627b6
ajlopez <webmaster@ajlopez.com>
2015-11-03 06:17:53 -0300

    Parser first simple tests

---
+++ Src/Husky.Tests/Compiler/ParserTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Husky.Tests.Compiler
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Husky.Compiler;
+
+    [TestClass]
+    public class ParserTests
+    {
+        [TestMethod]
+        public void NullTextAsNullExpression()
+        {
+            Parser parser = new Parser(null);
+
+            Assert.IsNull(parser.ParseExpression());
+        }
+
+        [TestMethod]
+        public void EmptyStringAsNullExpression()
+        {
+            Parser parser = new Parser(string.Empty);
+
+            Assert.IsNull(parser.ParseExpression());
+        }
+    }
+}
---
+++ Src/Husky/Compiler/Parser.cs
@@ -0,0 +1,20 @@
+﻿namespace Husky.Compiler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Husky.Expressions;
+
+    public class Parser
+    {
+        public Parser(string text)
+        {
+        }
+
+        public IExpression ParseExpression()
+        {
+            return null;
+        }
+    }
+}
