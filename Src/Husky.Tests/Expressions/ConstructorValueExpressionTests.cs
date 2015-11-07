5bd145e589236d982b93fc20912870a3a3d2dcb9
ajlopez <webmaster@ajlopez.com>
2015-11-07 12:12:54 -0300

    ConstructorValueExpression

---
+++ Src/Husky.Tests/Expressions/ConstructorValueExpressionTests.cs
@@ -0,0 +1,44 @@
+﻿namespace Husky.Tests.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Husky.Expressions;
+    using Husky.Functions;
+    using Husky.Types;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ConstructorValueExpressionTests
+    {
+        [TestMethod]
+        public void CreateWithType()
+        {
+            var vexpr = new ConstructorValueExpression(IntegerType.Instance);
+
+            Assert.AreSame(IntegerType.Instance, vexpr.Type);
+        }
+
+        [TestMethod]
+        public void Reduce()
+        {
+            var vexpr = new ConstructorValueExpression(IntegerType.Instance);
+
+            Assert.AreSame(vexpr, vexpr.Reduce());
+        }
+
+        [TestMethod]
+        public void Match()
+        {
+            var vexpr = new ConstructorValueExpression(IntegerType.Instance);
+            var vexpr2 = new ConstructorValueExpression(IntegerType.Instance);
+            var vexpr3 = new ConstructorValueExpression(DoubleType.Instance);
+
+            Assert.IsTrue(vexpr.Equals(vexpr));
+            Assert.IsFalse(vexpr.Equals(vexpr2));
+            Assert.IsFalse(vexpr.Equals(vexpr3));
+        }
+    }
+}
+
---
+++ Src/Husky/Expressions/ConstructorValueExpression.cs
@@ -0,0 +1,30 @@
+﻿namespace Husky.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Husky.Types;
+
+    public class ConstructorValueExpression : IExpression
+    {
+        private IType type;
+
+        public ConstructorValueExpression(IType type)
+        {
+            this.type = type;
+        }
+
+        public IType Type { get { return this.type; } }
+
+        public IExpression Reduce()
+        {
+            return this;
+        }
+
+        public bool Match(IExpression expr, Context<IExpression> ctx)
+        {
+            return this.Equals(expr);
+        }
+    }
+}
