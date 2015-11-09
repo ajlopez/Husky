41d3bf9525786d5ea6aaaef05369844928bc8fcf
ajlopez <webmaster@ajlopez.com>
2015-11-09 06:14:50 -0300

    Create TypeDeclaration with name

---
+++ Src/Husky.Tests/Declarations/TypeDeclarationTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Husky.Tests.Declarations
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Husky.Declarations;
+
+    [TestClass]
+    public class TypeDeclarationTests
+    {
+        [TestMethod]
+        public void CreateWithName()
+        {
+            var decl = new TypeDeclaration("Day");
+
+            Assert.AreEqual("Day", decl.Name);
+        }
+    }
+}
---
+++ Src/Husky/Declarations/TypeDeclaration.cs
@@ -0,0 +1,19 @@
+﻿namespace Husky.Declarations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class TypeDeclaration
+    {
+        private string name;
+
+        public TypeDeclaration(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name { get { return this.name; } }
+    }
+}
