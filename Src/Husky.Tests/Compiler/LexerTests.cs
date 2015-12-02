9d215d297e7cc9ded033db26c693ccd477c031cf
ajlopez <webmaster@ajlopez.com>
2015-12-02 06:56:41 -0300

    Get lambda operator

--- Src/Husky.Tests/Compiler/LexerTests.cs
+++ Src/Husky.Tests/Compiler/LexerTests.cs
@@ -220,6 +220,20 @@
         }
 
         [TestMethod]
+        public void GetLambdaOperator()
+        {
+            Lexer lexer = new Lexer("\\");
+
+            var token = lexer.NextToken();
+
+            Assert.IsNotNull(token);
+            Assert.AreEqual("\\", token.Value);
+            Assert.AreEqual(TokenType.Operator, token.Type);
+
+            Assert.IsNull(lexer.NextToken());
+        }
+
+        [TestMethod]
         public void GetDoubleColonAsOperator()
         {
             Lexer lexer = new Lexer("::");
