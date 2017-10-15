6e528e29acc73900bb76c51fe87a1517b46eefd9
ajlopez <webmaster@ajlopez.com>
2017-10-15 16:03:33 -0300

    ExecutionContext define value type

---
+++ Src/Husky.Tests/ExecutionContextTests.cs
@@ -0,0 +1,51 @@
+﻿namespace Husky.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Husky.Types;
+
+    [TestClass]
+    public class ExecutionContextTests
+    {
+        [TestMethod]
+        public void GetUndefinedValueType()
+        {
+            ExecutionContext context = new ExecutionContext();
+
+            var result = context.GetValueType("Foo");
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void SetAndGetValueValueType()
+        {
+            ExecutionContext context = new ExecutionContext();
+
+            context.DefineValue("Foo", IntegerType.Instance);
+            var result = context.GetValueType("Foo");
+
+            Assert.IsNotNull(result);
+            Assert.AreSame(IntegerType.Instance, result);
+        }
+
+        [TestMethod]
+        public void CannotDefineValueTypeTwice()
+        {
+            ExecutionContext context = new ExecutionContext();
+
+            context.DefineValue("Foo", IntegerType.Instance);
+
+            try
+            {
+                context.DefineValue("Foo", IntegerType.Instance);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(InvalidOperationException));
+                Assert.AreEqual("Value 'Foo' already defined", ex.Message);
+            }
+        }
+    }
+}
---
+++ Src/Husky/ExecutionContext.cs
@@ -0,0 +1,48 @@
+﻿namespace Husky
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Husky.Expressions;
+    using Husky.Types;
+
+    public class ExecutionContext
+    {
+        private Dictionary<string, IType> valueTypes = new Dictionary<string, IType>();
+
+        public void DefineValue(string name, IType type)
+        {
+            if (valueTypes.ContainsKey(name))
+                throw new InvalidOperationException(String.Format("Value '{0}' already defined", name));
+            
+            valueTypes[name] = type;
+        }
+
+        public IType GetValueType(string name)
+        {
+            if (!valueTypes.ContainsKey(name))
+                return null;
+
+            return valueTypes[name];
+        }
+
+        public void MapValue(String name, IExpression expr)
+        {
+        }
+
+        public IExpression GetValue(String name)
+        {
+            return null;
+        }
+
+        public void DefineType(string name, IType type)
+        {
+        }
+
+        public IType GetType(string name)
+        {
+            return null;
+        }
+    }
+}
